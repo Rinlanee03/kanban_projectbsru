cebc01b1cb34eaee7176a3cbe517fbe402cb66ed
Rinalanee03 <dangphoto.12@gmail.com>
2020-12-03 14:02:44 +0700

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
-  <div id="app">
-    <Kanban/>
-  </div>
+  <div id="app"><Kanban :data="data"/></div>
 </template>
 
 <script>
-import Kanban from "./components/Kanban"
+import Kanban from "./components/Kanban";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Kanban
-    
-  }
-}
+    Kanban,
+  },
+  data() {
+    return {
+      data: [
+        {
+          name: "todo",
+          color: "#6699FF",
+          tasks: [],
+        },
+        {
+          name: "todo",
+          color: "#6633FF",
+          tasks: [],
+        },
+        {
+          name: "todo",
+          color: "#CC33CC",
+          tasks: [],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
   height: 100vh;
-  
 }
 </style>
